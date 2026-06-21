0cefc109b1b1e77ea83150124b623a98d69645a5
guntimanoj <guntimanoj01@gmail.com>
2026-06-21 14:38:55 +0530

    reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,12 @@
+def reverse_string(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
+
+
+if __name__ == "__main__":
+    print(reverse_string(["h", "e", "l", "l", "o"]))
+    print(reverse_string(["H", "a", "n", "n", "a", "h"]))
